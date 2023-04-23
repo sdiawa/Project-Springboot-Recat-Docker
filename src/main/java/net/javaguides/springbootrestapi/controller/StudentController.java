302513a0521d20ec725b4528b05a4c29b5467ca5
sekou diawara <sekoukaderisc@yahoo.fr>
2023-04-23 16:37:35 +0200

    modifier StudentCOntroller

--- src/main/java/net/javaguides/springbootrestapi/controller/StudentController.java
+++ src/main/java/net/javaguides/springbootrestapi/controller/StudentController.java
@@ -1,64 +1,91 @@
 package net.javaguides.springbootrestapi.controller;
 
 import net.javaguides.springbootrestapi.bean.Student;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/students")
 public class StudentController {
 
-    //http:localhost:8080/student
+    // http://localhost:8080/student
     @GetMapping("/student")
-    public Student getStudent() {
+    public ResponseEntity<Student> getStudent() {
         Student student = new Student(
                 1,
                 "Sekou",
                 "Diawara",
                 "Mali"
         );
-        return student;
+        return ResponseEntity.ok()
+                .header("custom-header", "sekou")
+                .body(student);
     }
 
-    //http:localhost:8080/students
-    @GetMapping("/students")
-    public List<Student> getStudents() {
+    // http://localhost:8080/students
+    @GetMapping
+    public ResponseEntity<List<Student>> getStudents()  {
         List<Student> students = new ArrayList<>();
         students.add(new Student(1, "Sekou", "Diawara", "Mali"));
         students.add(new Student(2, "Oumou", "Dembele", "Paris"));
         students.add(new Student(3, "Moctar", "Diawara", "France"));
-        return students;
+        return ResponseEntity.ok(students);
     }
-    //utiliser le path avec rest API
-    //http:localhost:8080/students/1/Sekou/Diawara/Mali
 
-    @GetMapping("/students/{id}/{first-name}/{last-name}/{adresse}")
-    public Student studentPathVariable(@PathVariable("id") int studentId,
-                                       @PathVariable("first-name") String firstName,
-                                       @PathVariable("last-name") String lastName,
+    // utiliser le path avec REST API
+    // http://localhost:8080/students/1/Sekou/Diawara/Mali
+    @GetMapping("/{id}/{firstName}/{lastName}/{adresse}")
+    public ResponseEntity<Student>studentPathVariable(@PathVariable("id") int studentId,
+                                       @PathVariable("firstName") String firstName,
+                                       @PathVariable("lastName") String lastName,
                                        @PathVariable("adresse") String adresse) {
-        return new Student(studentId, firstName, lastName, adresse);
-
+         Student student = new Student(studentId, firstName, lastName, adresse);
+        return ResponseEntity.ok(student);
     }
 
-    //utiliser le path avec rest API
-    //http:localhost:8080/students/query?id=1&firstName=Sekou&lastName=Diawara&adresse=Mali
-
-    @GetMapping("/students/query")
-    public Student studentRequestVariable(@RequestParam int id,
+    // utiliser le path avec REST API
+    // http://localhost:8080/students/query?id=1&firstName=Sekou&lastName=Diawara&adresse=Mali
+    @GetMapping("/query")
+    public ResponseEntity<Student> studentRequestVariable(@RequestParam int id,
                                           @RequestParam String firstName,
                                           @RequestParam String lastName,
                                           @RequestParam String adresse) {
-        return new Student(id, firstName, lastName, adresse);
+        Student student= new Student(id, firstName, lastName, adresse);
+        return ResponseEntity.ok(student);
 
     }
 
-    // REST API HTTP POST Request
-    //@PostMapping et @RequestBody
-    @PostMapping("Students/create")
-    public Student createStudent(@RequestBody Student student) {
-        return service.saveStudent(student);
+    // REST API HTTP POST Request creating
+    // http://localhost:8080/students/create
+    @PostMapping("/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Student> createStudent(@RequestBody Student student) {
+        System.out.println(student.getId());
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        System.out.println(student.getAdresse());
+        return new ResponseEntity<>(student, HttpStatus.CREATED);
     }
+    // REST API HTTP POST Request update
+    // http://localhost:8080/students/update
+    @PostMapping("/{id}/update")
+    public ResponseEntity<Student> updateStudent(@RequestBody Student student, @PathVariable("id") int studentId) {
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        System.out.println(student.getAdresse());
+        return  ResponseEntity.ok(student);
+    }
+
+    // REST API HTTP POST Request remove
+    // http://localhost:8080/students/update
+    @DeleteMapping("/{id}/delete")
+   public ResponseEntity<String> deleteStudent(@PathVariable("id") int studentId){
+        System.out.println(studentId);
+        return ResponseEntity.ok("Etudiant supprimer avec succès");
+   }
+
 }
-
